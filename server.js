888315a5002612b0c2a952474ed619813189ece1
cityridetaxi <admincityridetaxi@gmail.com>
2026-06-08 23:23:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -873,10 +873,13 @@ cron.schedule('0 * * * *', async () => {
 async function startServer() {
     try {
         await initDB();
+
         const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
+
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on http://0.0.0.0:${PORT}`);
         });
+
     } catch (err) {
         console.error('CRITICAL ERROR during startup:', err);
         process.exit(1);
